2b85bd0a25a04d22a6805f8509fbf97c986b7814
Linet L.K. Kogo <linet.kogo@vault.lishailabs.com>
2026-02-19 17:02:42 +0300

    Added backend

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+import userRoutes from "./routes/user.routes.js";
+
+
+const app = express();
+
+dotenv.config(); 
+
+const PORT = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Uzima API");
+});
+app.use("/users", userRoutes);
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection failed:", error);
+    process.exit(1);
+  });
