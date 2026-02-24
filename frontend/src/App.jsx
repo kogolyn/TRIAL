603400a8f60c929a2ef90d61ad462547faf09d9a
faithaura <ateshfaith@gmail.com>
2026-02-24 12:43:52 +0300

    corrected hospital routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -595,7 +595,7 @@ function App() {
         />
 
         <Route
-          path="/dashboard"
+          path="/dashboard/*"
           element={
             <ProtectedRoute user={user} role="medical">
               <DashboardRoutes />
